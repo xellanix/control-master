730069954d0fcdb77f190d72607e1e6255e66f14
xellanix <107340353+xellanix@users.noreply.github.com>
2026-04-24 14:54:53 +0700

    feat: init server + socket.io

---
+++ server/index.ts
@@ -0,0 +1,75 @@
+import { serve, file } from "bun";
+import { dirname, join } from "path";
+import open from "open";
+import { engine, SERVER_PORT, FRONTEND_PORT } from "$/socket";
+
+const isProd = process.env.NODE_ENV === "production";
+const FRONTEND_DIST = join(dirname(process.execPath), "frontend");
+const { fetch, ...socketEngineHandler } = engine.handler();
+
+declare const VERSION: string;
+
+console.log("┌────────────────────────────────┐");
+console.log("│ Xellanix ControlMaster         │");
+{
+    const len = 22 - VERSION.length;
+    console.log(`│ Version ${VERSION}${len > 0 ? " ".repeat(len) : ""} │`);
+}
+console.log("├────────────────────────────────┤");
+
+serve({
+    ...socketEngineHandler,
+
+    // Set it at the end of the server configuration options
+    // so that all custom settings are fully applied
+    // and not overridden by the configuration
+    // from the libraries being used.
+    port: SERVER_PORT,
+    async fetch(req, server) {
+        const url = new URL(req.url);
+        const path = url.pathname;
+
+        if (path.startsWith(engine.opts.path)) {
+            return engine.handleRequest(req, server);
+        }
+
+        if (!isProd) {
+            return new Response(
+                "Bun Backend: Running in DEV mode. Please use the Vite dev server to view the frontend.",
+                { headers: { "Content-Type": "text/plain" } },
+            );
+        }
+
+        let reqPath = decodeURIComponent(path);
+        if (reqPath === "/") reqPath = "/index.html";
+
+        const targetPath = join(FRONTEND_DIST, reqPath);
+        if (!targetPath.startsWith(FRONTEND_DIST)) {
+            return new Response("Forbidden: Invalid Path", { status: 403 });
+        }
+
+        let requestedFile = file(targetPath);
+        if (!(await requestedFile.exists())) {
+            requestedFile = file(join(FRONTEND_DIST, "index.html"));
+
+            // Safety check: if index.html is completely missing, return a clean 404
+            if (!(await requestedFile.exists())) {
+                return new Response(
+                    "Frontend files not found. Ensure the 'frontend' folder is located in the same directory as this executable.",
+                    { status: 404, headers: { "Content-Type": "text/plain" } },
+                );
+            }
+        }
+
+        return new Response(requestedFile);
+    },
+});
+
+console.log(`│ Server: http://localhost:${SERVER_PORT} │`);
+console.log(`│ Mode  : ${isProd ? "production " : "development"}            │`);
+console.log("└────────────────────────────────┘");
+
+if (process.env.ALREADY_OPENED !== "true") {
+    void open(`http://localhost:${isProd ? SERVER_PORT : FRONTEND_PORT}`);
+    process.env.ALREADY_OPENED = "true";
+}
---
+++ server/socket.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { Server } from "socket.io";
+import { Server as Engine } from "@socket.io/bun-engine";
+import { hostname } from "os";
+
+const SERVER_PORT = 26041;
+const FRONTEND_PORT = 26042;
+const isProd = process.env.NODE_ENV === "production";
+
+const io = new Server({ transports: ["websocket"] });
+const engine = new Engine({
+    path: "/api/socket_io/",
+    cors: {
+        origin: isProd ? false : `http://localhost:${FRONTEND_PORT}`,
+    },
+});
+io.bind(engine);
+io.on("connection", (socket) => {
+    console.log("✅ Client connected:", socket.id);
+
+    socket.emit("server:socket:device:name", hostname());
+    console.log("Device name:", hostname());
+
+    socket.on("disconnect", () => {
+        const str = `❌ Client disconnected: ${socket.id}`;
+        console.log(str);
+        console.log("-".repeat(Bun.stringWidth(str)));
+    });
+});
+
+export { engine, SERVER_PORT, FRONTEND_PORT };
